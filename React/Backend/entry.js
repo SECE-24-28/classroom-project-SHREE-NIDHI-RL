7e19edb2a94eda49c0e762e15d9295f07a949613
SHREE-NIDHI-RL <shreenidhi.rl2024csecs@sece.ac.in>
2025-12-15 15:33:58 +0530

    Added Day 10 internship work

--- React/Backend/entry.js
+++ React/Backend/entry.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app=express()
-const PORT=8001
+const express = require('express');
+const mdb=require('mongoose');
+const app=express();
+const PORT=8001;
+mdb.connect('mongodb://127.0.0.1:27017/MERN').then(()=>{
+    console.log("Mongodb connection successfull")
+}).catch((err)=>{
+    console.log("Mongodb connection unsuccessful",err)
+})
 app.get('/',(req,res)=>{
     res.send("Welcome to backend server")
 })
@@ -16,4 +22,14 @@ app.get('/static',(req,res)=>{
     })
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
+})
+app.post('/signup',(req, res)=>{
+        const{email,username,password} = req.body;
+        const newSignup = {
+        email: email,
+        username: username,
+        password: password
+    }
+    newSignup.save
+    res.json({"Message":"Signup successfull"})
 })
